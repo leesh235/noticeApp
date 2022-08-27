12d7168e65ef9258dde8c9cf6c3cb3d9bb056241
leesh235 <leeshgh235@gmail.com>
2022-08-27 11:22:03 +0900

    DnD setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-    return <div className="App">App</div>;
+    const [dataList, setDataList] = useState([1, 2, 3]);
+    const [dnd, setDnd] = useState({
+        draggedFrom: null,
+        draggedTo: null,
+        isDragging: false,
+        originalOrder: [],
+        updatedOrder: [],
+    });
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDragStart = (e) => {};
+
+    const handleDragEnd = (e) => {};
+
+    const handleDrop = (e) => {};
+
+    const handleDragLeave = (e) => {};
+
+    const handleDragEnter = (e) => {};
+
+    useEffect(() => {}, []);
+
+    return (
+        <div className="app">
+            <ul className="list" onDragOver={handleDragOver}>
+                {dataList.map((val, idx) => {
+                    return (
+                        <li
+                            className="draggable item"
+                            key={idx}
+                            data-position={idx}
+                            draggable="true"
+                            onDragStart={handleDragStart}
+                            onDragOver={handleDragOver}
+                            onDragLeave={handleDragLeave}
+                            onDrop={handleDrop}
+                            onDragEnter={handleDragEnter}
+                            onDragEnd={handleDragEnd}
+                        >
+                            {val}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
