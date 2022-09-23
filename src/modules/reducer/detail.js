3c4333a33b2ad5d2818c8bc8d0947a0c2400a9b7
leesh235 <leeshgh235@gmail.com>
2022-09-23 23:32:42 +0900

    게시글 변경 로직추가

--- src/modules/reducer/detail.js
+++ src/modules/reducer/detail.js
@@ -7,15 +7,35 @@ const reducer = (state = initState, action) => {
     const { type, data } = action;
     switch (type) {
         case GETDETAIL:
-            return { ...state, data };
+            return { ...state, [data.id]: db.detail[data.id] };
         case PUTDETAIL:
-            const newList = state[data.parent].map((val, idx) => {
-                if (val.id === data.detail.id) return { ...data.detail };
-                else return { ...val };
-            });
+            let contents = state[data.id].contents;
+            if (data?.value !== "") {
+                const newContents = contents.map((val) => {
+                    if (val.id === data.contentId)
+                        return { ...val, value: data.value };
+                    else return { ...val };
+                });
+                console.log(newContents);
+                contents = newContents;
+            } else if (data.value === "") {
+                contents = [
+                    ...contents,
+                    {
+                        id: state[data.id].contents.length,
+                        value: data.value,
+                    },
+                ];
+            }
+            console.log(contents);
             return {
                 ...state,
-                [data.parent]: [...newList],
+                [data.id]: {
+                    ...state[data.id],
+                    title: data.title || state[data.id].title,
+                    modifyAt: data.modifyAt || state[data.id].modifyAt,
+                    contents,
+                },
             };
         default:
             return state;
