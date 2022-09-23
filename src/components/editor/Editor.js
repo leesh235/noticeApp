38f27d1c1f45b978037ed98a38bb6ac2f430892a
leesh235 <leeshgh235@gmail.com>
2022-09-23 23:35:10 +0900

    custom input 추가 및 배열 컨트롤

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,87 +1,29 @@
 import "./Editor.css";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
+import { useSelector } from "react-redux";
+import { InputController } from "./InputController/InputController";
 
-const menuList = ["기본", "할일", "리스트"];
-
-export const Editor = () => {
+export const Editor = ({ id }) => {
     const editorRef = useRef(null);
-    const [open, setOpen] = useState(false);
-    const [type, setType] = useState(0);
-    const [list, setList] = useState([1]);
 
-    const _open = (e) => {
-        setOpen(!open);
-    };
-
-    const _changeType = (e) => {
-        e.stopPropagation();
-        setType(Number(e.currentTarget.id));
-        setOpen(false);
-    };
-
-    const _onInput = (e) => {
-        console.log(editorRef.current);
-    };
-
-    const _onKeyDown = (e) => {
-        console.log(e.currentTarget.id);
-        if (e.nativeEvent.isComposing) return;
-        if (e.key === "Enter" && !e.shiftKey) {
-            setList((prev) => prev.concat([list + 1]));
-            e.currentTarget.blur();
-        }
-    };
+    const contents = useSelector((state) => {
+        return state.detail[id]?.contents;
+    });
 
     useEffect(() => {
-        if (list.length === 1) return;
-        const nextInput =
-            document.querySelector("#editor").childNodes[list.length - 1]
-                .childNodes[2];
-        while (nextInput.hasChildNodes()) {
-            nextInput.removeChild(nextInput.firstChild);
-        }
-        nextInput.focus();
-    }, [list]);
+        console.log(contents);
+    }, [contents]);
 
     return (
         <section id="editor" ref={editorRef}>
-            {list.map((val, idx) => {
+            {contents?.map((val, idx) => {
                 return (
-                    <div className="line_wrapper" key={idx}>
-                        <div className="input_type_btn" onClick={_open}>
-                            {open && (
-                                <ul className="input_type_menu">
-                                    {menuList.map((val, index) => {
-                                        return (
-                                            <li
-                                                key={index}
-                                                id={index}
-                                                className="input_type_item"
-                                                onClick={_changeType}
-                                            >
-                                                {val}
-                                            </li>
-                                        );
-                                    })}
-                                </ul>
-                            )}
-                        </div>
-                        <div className="dnd_btn"></div>
-                        <div
-                            id={idx}
-                            className="input_type"
-                            contentEditable={true}
-                            // onInput={_onInput}
-                            onKeyDown={_onKeyDown}
-                        />
-                        {/* {type === 1 && (
-                            <div id="check_input">
-                            <input type="checkbox" onChange={_changeType} />
-                            <input placeholder={"할일"} />
-                            </div>
-                            )}
-                        {type === 2 && <input placeholder={"리스트"} />} */}
-                    </div>
+                    <InputController
+                        key={val.id}
+                        detailId={id}
+                        value={val}
+                        isLast={idx === contents?.length - 1}
+                    />
                 );
             })}
         </section>
