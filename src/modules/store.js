5a8580e73464e605b3a0eb3fd2b2e88f0e390226
leesh235 <leeshgh235@gmail.com>
2022-09-23 23:32:15 +0900

    detail 리듀서 추가

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from "redux";
 //reducer
 import list from "./reducer/list";
+import detail from "./reducer/detail";
 
-const rootReducer = combineReducers({ list });
+const rootReducer = combineReducers({ list, detail });
 
 const store = createStore(rootReducer);
 
